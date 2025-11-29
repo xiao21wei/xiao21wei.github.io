9f48664c1a1072be62799c73d606fee0fbbe5dca
weiwei <2889811702@qq.com>
2025-11-29 18:00:17 +0800

    Site updated: 2025-11-29 18:00:17

---
+++ anzhiyu/random.js
@@ -0,0 +1,3 @@
+var posts=["2025/11/28/hello-world/","2025/11/29/build-guide/","2025/04/09/update-list/"];function toRandomPost(){
+    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
+  };
